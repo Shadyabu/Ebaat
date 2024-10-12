80aa5edf423aa528d2319f9b46b4d032b26f3e7c
Karol Bura <virtualreader76@gmail.com>
2024-10-12 15:52:36 +0100

    added test binding for helloworld contract

---
+++ packages/hello_world/dist/index.js
@@ -0,0 +1,26 @@
+import { Buffer } from "buffer";
+import { Client as ContractClient, Spec as ContractSpec, } from '@stellar/stellar-sdk/contract';
+export * from '@stellar/stellar-sdk';
+export * as contract from '@stellar/stellar-sdk/contract';
+export * as rpc from '@stellar/stellar-sdk/rpc';
+if (typeof window !== 'undefined') {
+    //@ts-ignore Buffer exists
+    window.Buffer = window.Buffer || Buffer;
+}
+export const networks = {
+    testnet: {
+        networkPassphrase: "Test SDF Network ; September 2015",
+        contractId: "CASDJ7LIGJLK2JXAI4BB6CAA42AFPTQ7OLXA2HF2WGTAZJM6MNZ75SYN",
+    }
+};
+export const Errors = {};
+export class Client extends ContractClient {
+    options;
+    constructor(options) {
+        super(new ContractSpec(["AAAAAAAAAAAAAAAFaGVsbG8AAAAAAAABAAAAAAAAAAJ0bwAAAAAAEAAAAAEAAAPqAAAAEA=="]), options);
+        this.options = options;
+    }
+    fromJSON = {
+        hello: (this.txFromJSON)
+    };
+}
