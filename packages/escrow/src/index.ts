f08712a6b94a8d2bb77283e4b63e8ec295774ddb
Karol Bura <virtualreader76@gmail.com>
2024-10-13 10:12:43 +0100

    created package escrow

---
+++ packages/escrow/src/index.ts
@@ -0,0 +1,139 @@
+import { Buffer } from "buffer";
+import { Address } from '@stellar/stellar-sdk';
+import {
+  AssembledTransaction,
+  Client as ContractClient,
+  ClientOptions as ContractClientOptions,
+  Result,
+  Spec as ContractSpec,
+} from '@stellar/stellar-sdk/contract';
+import type {
+  u32,
+  i32,
+  u64,
+  i64,
+  u128,
+  i128,
+  u256,
+  i256,
+  Option,
+  Typepoint,
+  Duration,
+} from '@stellar/stellar-sdk/contract';
+export * from '@stellar/stellar-sdk'
+export * as contract from '@stellar/stellar-sdk/contract'
+export * as rpc from '@stellar/stellar-sdk/rpc'
+
+if (typeof window !== 'undefined') {
+  //@ts-ignore Buffer exists
+  window.Buffer = window.Buffer || Buffer;
+}
+
+
+export const networks = {
+  testnet: {
+    networkPassphrase: "Test SDF Network ; September 2015",
+    contractId: "CDE2UZBDQR3M745AUOLIFQHNQHHDFS6ULPSX6Y2ODSS5X36KLPISVW2I",
+  }
+} as const
+
+export type DataKey = {tag: "Init", values: void} | {tag: "Balance", values: void} | {tag: "ClaimableBalance", values: void};
+
+
+export interface Balance {
+  amount: i128;
+  releaser: string;
+  token: string;
+}
+
+
+export interface ClaimableBalance {
+  balance: Balance;
+  claimant: string;
+}
+
+export const Errors = {
+
+}
+
+export interface Client {
+  /**
+   * Construct and simulate a deposit transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   * Increment increments an internal counter, and returns the value.
+   */
+  deposit: ({from, token, amount}: {from: string, token: string, amount: i128}, options?: {
+    /**
+     * The fee to pay for the transaction. Default: BASE_FEE
+     */
+    fee?: number;
+
+    /**
+     * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
+     */
+    timeoutInSeconds?: number;
+
+    /**
+     * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
+     */
+    simulate?: boolean;
+  }) => Promise<AssembledTransaction<null>>
+
+  /**
+   * Construct and simulate a claim transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  claim: ({claimant}: {claimant: string}, options?: {
+    /**
+     * The fee to pay for the transaction. Default: BASE_FEE
+     */
+    fee?: number;
+
+    /**
+     * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
+     */
+    timeoutInSeconds?: number;
+
+    /**
+     * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
+     */
+    simulate?: boolean;
+  }) => Promise<AssembledTransaction<null>>
+
+  /**
+   * Construct and simulate a release transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  release: ({releaser}: {releaser: string}, options?: {
+    /**
+     * The fee to pay for the transaction. Default: BASE_FEE
+     */
+    fee?: number;
+
+    /**
+     * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
+     */
+    timeoutInSeconds?: number;
+
+    /**
+     * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
+     */
+    simulate?: boolean;
+  }) => Promise<AssembledTransaction<null>>
+
+}
+export class Client extends ContractClient {
+  constructor(public readonly options: ContractClientOptions) {
+    super(
+      new ContractSpec([ "AAAAAgAAAAAAAAAAAAAAB0RhdGFLZXkAAAAAAwAAAAAAAAAAAAAABEluaXQAAAAAAAAAAAAAAAdCYWxhbmNlAAAAAAAAAAAAAAAAEENsYWltYWJsZUJhbGFuY2U=",
+        "AAAAAQAAAAAAAAAAAAAAB0JhbGFuY2UAAAAAAwAAAAAAAAAGYW1vdW50AAAAAAALAAAAAAAAAAhyZWxlYXNlcgAAABMAAAAAAAAABXRva2VuAAAAAAAAEw==",
+        "AAAAAQAAAAAAAAAAAAAAEENsYWltYWJsZUJhbGFuY2UAAAACAAAAAAAAAAdiYWxhbmNlAAAAB9AAAAAHQmFsYW5jZQAAAAAAAAAACGNsYWltYW50AAAAEw==",
+        "AAAAAAAAAEBJbmNyZW1lbnQgaW5jcmVtZW50cyBhbiBpbnRlcm5hbCBjb3VudGVyLCBhbmQgcmV0dXJucyB0aGUgdmFsdWUuAAAAB2RlcG9zaXQAAAAAAwAAAAAAAAAEZnJvbQAAABMAAAAAAAAABXRva2VuAAAAAAAAEwAAAAAAAAAGYW1vdW50AAAAAAALAAAAAA==",
+        "AAAAAAAAAAAAAAAFY2xhaW0AAAAAAAABAAAAAAAAAAhjbGFpbWFudAAAABMAAAAA",
+        "AAAAAAAAAAAAAAAHcmVsZWFzZQAAAAABAAAAAAAAAAhyZWxlYXNlcgAAABMAAAAA" ]),
+      options
+    )
+  }
+  public readonly fromJSON = {
+    deposit: this.txFromJSON<null>,
+        claim: this.txFromJSON<null>,
+        release: this.txFromJSON<null>
+  }
+}
